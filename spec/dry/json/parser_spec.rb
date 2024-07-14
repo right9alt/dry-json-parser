70cc6a45aa4e5723baa607548434359e1b678559
Данилин Даниил <d-danilin@groupprice.ru>
2024-07-14 13:39:32 +0300

    initial commit

---
+++ spec/dry/json/parser_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Dry::Json::Parser do
+  it "has a version number" do
+    expect(Dry::Json::Parser::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
